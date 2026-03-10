b00ffab132fc8db99f6a7a9ed4cab2bc00695b60
noname <stanag.work@gmail.com>
2026-03-10 15:52:18 +0000

    fixed-movement

--- player.py
+++ player.py
@@ -25,7 +25,7 @@ class Player(CircleShape):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
             self.move(dt)
-        if keys[pygame.K_d]:
+        if keys[pygame.K_s]:
             self.move(dt * -1)
         if keys[pygame.K_a]:
             self.rotate(dt * -1)                    
